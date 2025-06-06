bfe2e7d3341b3343d7e6c11eb797f8de5c6c94e8
anoopajit <46787493+anoopajit@users.noreply.github.com>
2025-06-06 23:17:20 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,13 +1,4 @@
 terraform {
   required_version = ">= 1.0"
   
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
-}
+   }
